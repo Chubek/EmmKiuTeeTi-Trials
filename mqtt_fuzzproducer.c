b0621d28e862643c680a518d0f00278289ecb46b
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-05-02 17:50:13 +0330

    added typedefs and signatures

--- mqtt_fuzzproducer.c
+++ mqtt_fuzzproducer.c
@@ -12,10 +12,27 @@
 
 #define O_CREAT 64
 
-typedef char *mqtt_packetbytes_t;
+typedef void *mqtt_voidptr_t;
+typedef void *mqtt_memaddr_t;
+typedef void mqtt_nonyield_t;
+
+typedef signed char *mqtt_filepath_t;
+typedef signed char *mqtt_hostaddr_t;
+typedef signed char mqtt_yield_t;
+typedef signed int mqtt_procid_t;
+typedef signed int mqtt_filedsc_t;
+typedef signed int mqtt_sockdsc_t;
+typedef signed int mqtt_flags_t;
+typedef signed int mqtt_exitcode_t;
+
+typedef unsigned char *mqtt_packetbytes_t;
+typedef unsigned char mqtt_pubackres_t;
+typedef unsigned short mqtt_netport_t;
+typedef unsigned int mqtt_ipv4_t;
+typedef unsigned int mqtt_readlen_t;
+typedef unsigned int mqtt_writelen_t;
 typedef unsigned long mqtt_packetlen_t;
 typedef unsigned long mqtt_packetnum_t;
-typedef char mqtt_pubackres_t;
 typedef unsigned long mqtt_pubackoff_t;
 
 typedef struct MQTTFuzzPacket {
@@ -23,48 +40,42 @@ typedef struct MQTTFuzzPacket {
     mqtt_packetbytes_t packet_bytes;
 } mqtt_packet_s;
 
-typedef struct MQTTFuzzPacketColl {
-    mqtt_packetnum_t numpublishpackets; 
-    mqtt_packet_s *publishpackets;
+typedef struct MQTTFuzzPeripheryPackets {
     mqtt_packet_s connectpacket;
     mqtt_packet_s connackpacket;
     mqtt_packet_s disconnpacket;
     mqtt_packet_s pubackpacket;
-} mqtt_packcoll_s;
+} mqtt_peripherypackets_s;
 
+typedef struct MQTTFuzzPublishPackets {
+    mqtt_packetnum_t publishpacketsnum; 
+    mqtt_packet_s *publishpackets;
+} mqtt_publishpackets_s;
 
-typedef struct MQTTFuzzPubackRes {
-    mqtt_pubackoff_t writeoffse;
-    mqtt_packetnum_t numpubackpackets;
-    mqtt_pubackres_t *pubackresults;
-} mqtt_pubacks_t;
+typedef struct MQTTFuzzTest {
+    mqtt_peripherypacket_s peripherypackets;
+    mqtt_publishpackets_s publishpackets;
+} mqtt_fuzztest_s;
 
-extern int read_from_file(int fd, void *dst, int readcount, int bytecount);
-extern int open_file_path(char *path, int flags);
-extern void close_file_desc(int fd);
-extern void exit_from_app(int code);
-extern void error_out_from_app(int code);
-extern int convert_hostaddr_to_network_int(char *host, unsigned int *ipv4);
-extern int read_single_packet_from_file(int fd, mqtt_packet_s *packet);
-extern void free_single_packet_bytes(mqtt_packet_s *packet);
-extern char *memorymap_file_shared(int fd, mqtt_packetnum_t pubacksnum, mqtt_pubackoff_t pubacksoffset);
-
-
-mqtt_packcoll_s *parse_packet_file(char *filepath) {
-    mqtt_packcoll_s packetscoll;
-}
+extern mqtt_yield_t read_from_file(mqtt_filedsc_t fd, mqtt_voidptr_t dst, mqtt_readlen_t readcount, mqtt_readlen_t bytecount);
+extern mqtt_yield_t open_file_path(mqtt_filepath_t path, mqtt_flags_t flags);
+extern mqtt_nonyield_t close_file_desc(mqtt_filedsc_t fd);
+extern mqtt_nonyield_t exit_from_app(mqtt_exitcode_t code);
+extern mqtt_yield_t read_single_packet_from_file(mqtt_filedsc_t fd, mqtt_packet_s *packet);
+extern mqtt_nonyield_t free_single_packet_bytes(mqtt_packet_s *packet);
+extern mqtt_memaddr_t memorymap_file_shared(mqtt_filedsc_t fd, mqtt_packetnum_t pubacksnum, mqtt_pubackoff_t pubacksoffset);
+extern mqtt_yield_t send_packet_to_broker(mqtt_sockdsc_t sock, mqtt_packet_s *packet);
+extern mqtt_yield_t receive_packet_from_broker(mqtt_sockdsc_t sock, mqtt_packet_s *packet);
+extern mqtt_yield_t convert_hostaddr_to_netbyteorder(mqtt_hostaddr_t host, mqtt_ipv4_t *ipv4conv);
+extern mqtt_netport_t netport_to_netbyteorder(mqtt_netport_t port);
+extern mqtt_sockdsc_t open_socket_and_connect(mqtt_ipv4_t ipv4addr, mqtt_netport_t port);
+extern mqtt_yield_t write_packet_to_mmaped_file(mqtt_filedsc_t fd, mqtt_packet_s *packet);
 
 
 int main() {
-   int fd = open_file_path("res.bin", O_CREAT);
-   mqtt_pubacks_t pubacks;
-   pubacks.writeoffset = 10;
-   pubacks.numpubackpackets = 3;
-   pubacks.pubackresults = calloc(3, 1);
-   pubacks.pubackresults[0] = 'F';
-   pubacks.pubackresults[1] = 'S';
-   pubacks.pubackresults[2] = 'F';
-   write_pubacks_to_file(fd, &pubacks);
-   free(pubacks.pubackresults);
-   close_file_desc(fd);
+   unsigned short port = 42111;
+   unsigned short netport = htons(port);
+   mqtt_netport_t cstport = netport_to_netbyteorder(port);
+
+   printf("%hu %hu\n", netport, cstport);
 }
